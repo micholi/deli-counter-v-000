f55fe393e0695a8120a4f76a6ba7d7adeb82220a
Michelle Olivieri <michelleo72@gmail.com>
2018-03-20 21:14:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,8 +16,8 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-    puts "Welcome, #{name}. You are number 1 in line."
-  # katz_deli.each_with_index do |name, index|
-    #number = index + 1
-  #end
+  katz_deli.each_with_index do |name, index|
+    number = index + 1
+    puts "Welcome, #{name}. You are number #{number} in line."
+end
 end
