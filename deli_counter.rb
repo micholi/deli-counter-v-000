f4c324706abc7e51781ab34b23b562d88a9b809b
Michelle Olivieri <michelleo72@gmail.com>
2018-03-20 20:33:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,11 +10,11 @@ def line(katz_deli)
         number = index + 1
            intro = "The line is currently: #{number}. #{name} "
               message = intro << "#{number}. #{name} "
-puts message
+
       end
 
     end
-
+puts message
   end
 end
 
