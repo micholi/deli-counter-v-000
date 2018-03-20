95957ba7d3742fad802d3a813e19352b3bfa990b
Michelle Olivieri <michelleo72@gmail.com>
2018-03-20 19:51:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,10 @@
 katz_deli = []
 
 def line(katz_deli)
-    puts "The line is currently empty." if katz_deli.size == 0
+  if katz_deli.size == 0
+    puts "The line is currently empty." 
+  else
+    
   end
 end
 
