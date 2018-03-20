3dbb0b934b53847a15ea5e014f8ae21cd0414c71
Michelle Olivieri <michelleo72@gmail.com>
2018-03-20 20:59:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,11 +5,10 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    # message = "The line is currently:"
+    message = "The line is currently:"
       katz_deli.each do |name|
         customer = katz_deli.each_with_index do |name, index|
           number = index + 1
-message = "The line is currently:"
            message << " #{number}. #{name}"
 
       end
@@ -17,7 +16,7 @@ message = "The line is currently:"
     end
 
   end
-  
+
 end
 
 def take_a_number(katz_deli, name)
