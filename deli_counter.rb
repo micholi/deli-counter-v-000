d3c74900c4f508b9a5756553c65af23e001a3080
Michelle Olivieri <michelleo72@gmail.com>
2018-03-20 19:59:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,10 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.size == 0
-    puts "The line is currently empty." 
+    puts "The line is currently empty."
   else
+    katz_deli.each do |name|
+
     puts "The line is currently: "
   end
 end
