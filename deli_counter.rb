c7bc7bf0345d2df12d3c72ae211d3ba0808bcbf5
Michelle Olivieri <michelleo72@gmail.com>
2018-03-20 20:30:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(katz_deli)
     katz_deli.each do |name|
       customer = katz_deli.each_with_index do |name, index|
         number = index + 1
-           intro = "The line is currently: "
+           intro = "The line is currently: #{number}. #{name} "
               message = intro << "#{number}. #{name} "
 puts message
       end
